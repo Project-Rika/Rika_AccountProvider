65ccd50198b134b02204ade3595f939a7b8b4ec4
Nike <nikesjolin@outlook.com>
2024-10-31 09:54:32 +0100

    UserRepository UpdateUserAsync finished

--- AccountProvider/Repositories/UserRepository.cs
+++ AccountProvider/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AccountProvider.Context;
 using AccountProvider.Entities;
 using AccountProvider.Interfaces;
+using System.Diagnostics;
 
 namespace AccountProvider.Repositories;
 
@@ -8,8 +9,26 @@ public class UserRepository(DataContext context) : IUserRepository
 {
     private readonly DataContext _context = context;
 
-    public Task<UserEntity> UpdateUserAsync(UserEntity userEntity)
+    public async Task<UserEntity?> UpdateUserAsync(UserEntity userEntity)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (userEntity != null)
+            {
+                _context.Users.Update(userEntity);
+                var result = await _context.SaveChangesAsync();
+
+                if (result > 0)
+                {
+                    return userEntity;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+           Debug.WriteLine("ERROR :: " + ex.Message);
+        }
+
+        return null;
     }
 }
